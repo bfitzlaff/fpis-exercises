a0a7cc0660678af283ae4bf737e74efe4c838444
Brandon Fitzlaff <brandon.fitzlaff@banno.com>
2017-03-31 10:04:23 -0500

    Complete chapter 2 exercises

--- src/main/scala/Chapter2.scala
+++ src/main/scala/Chapter2.scala
@@ -1,15 +1,35 @@
-package fpis.ch1
+package fpis.exercises
 
-object Ch1Exercises {
+import scala.annotation.tailrec
+
+object Chapter2 {
   //2.1
-  def fib(n: Int) = {
-    def fib(n: Int, a: Int) = {
-      n match {
-        case 0 => a
-        case 1 => a + 1
-        case n => fib(n - 1, a + n)
-      }
-    }
-    fib(n, 0)
+  def fib(n: Int): Int = {
+    @tailrec
+    def fib(n: Int, p: Int, a: Int): Int = if(n < 1) a else fib(n - 1, a, a + p)
+
+    fib(n, 1, 0)
   }
+
+  //2.2
+  def isSorted[A](as: Array[A], ordered: (A,A) => Boolean): Boolean = {
+    val max = as.length - 1
+
+    @tailrec
+    def isSorted(index: Int): Boolean =
+      if(index > max) true
+      else if(ordered(as(index), as(index + 1))) isSorted(index + 1)
+      else false
+
+    isSorted(0)
+  }
+
+  //2.3
+  def curry[A,B,C](f: (A, B) => C): A => (B => C) = (a) => f(a, _)
+
+  //2.4
+  def uncurry[A,B,C](f: A => B => C): (A, B) => C = f(_)(_)
+
+  //2.5
+  def compose[A,B,C](f: B => C, g: A => B): A => C =  (a) => f(g(a))
 }
